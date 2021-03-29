0620be8fae13a28e93e3d2c1720dea76e3f199ff
Doruk Dorkodu <dorkodu@gmail.com>
2021-03-29 11:29:48 +0300

    Added construct()

--- source/Dorkodu/Outstor/DBConnection.php
+++ source/Dorkodu/Outstor/DBConnection.php
@@ -20,6 +20,34 @@
     public $charset;
     public $collation;
 
+    /**
+     * Database Connection Constructor.
+     *
+     * @param array $config
+     */
+    public function __construct( string $driver = 'mysql',
+                                 string $host = 'localhost',
+                                 string $database,
+                                 string $user,
+                                 string $password,
+                                 string $port = '',
+                                 string $charset = 'utf8',
+                                 string $collation = 'utf8_general_ci' )
+    {
+      $this->driver = $driver;
+      $this->host = $host;
+      $this->user = $user;
+      $this->password = $password;
+      $this->database = $database;
+
+      $this->port = ($this->port !== '')
+                    ? $port
+                    : (strstr($host, ':') ? explode(':', $host)[1] : '');
+
+      $this->charset = $charset;
+      $this->collation = $collation;
+    }
+
     public function __get($name)
     {
       return $this->$name;
