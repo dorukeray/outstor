c512f148f019f79d8b2c5d61f2fa076ee0a8d494
Doruk Dorkodu <dorkodu@gmail.com>
2021-03-29 11:29:06 +0300

    Added DBConnection

---
+++ source/Dorkodu/Outstor/DBConnection.php
@@ -0,0 +1,14 @@
+<?php
+  namespace Dorkodu\Outstor;
+
+  /**
+   * DBConnection - SQL Database Connection Value Object
+   *
+   * @author Doruk Eray (@dorkodu) <doruk@dorkodu.com>
+   * @url <https://github.com/dorukdorkodu/outstor>
+   * @license The MIT License (MIT) - <http://opensource.org/licenses/MIT>
+   */
+  class DBConnection
+  {
+
+  }
