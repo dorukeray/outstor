46f7ea07226f79c09cd4db06d4edc00affbad16e
Doruk Dorkodu <dorkodu@gmail.com>
2021-03-31 14:57:04 +0300

    Fİnally, the DB class is done.

---
+++ source/Dorkodu/Outstor/DB.php
@@ -0,0 +1,22 @@
+<?php
+  namespace Dorkodu\Outstor;
+
+  use Dorkodu\Outstor\{
+                        IConnection,
+                        SqliteConnection, 
+                        MysqlConnection,
+                      };
+
+  use PDO, PDOException, Exception, PDOStatement;
+
+  /**
+   * DB - the Database class for Outstor library
+   *
+   * @author  Doruk Eray (@dorkodu) <doruk@dorkodu.com>
+   * @url      <https://github.com/dorukdorkodu/outstor>
+   * @license  The MIT License (MIT) - <http://opensource.org/licenses/MIT>
+   */
+  class DB
+  {
+    
+  }
