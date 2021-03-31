aeb551205a8579df6a247d0811c519ca0b66f38a
Doruk Dorkodu <dorkodu@gmail.com>
2021-03-31 14:57:50 +0300

    Added properties

--- source/Dorkodu/Outstor/DB.php
+++ source/Dorkodu/Outstor/DB.php
@@ -18,5 +18,48 @@
    */
   class DB
   {
+    /**
+     * @var PDO|null
+     */
+    public $pdo = null;
     
+    /**
+     * @var IConnection. 
+     */
+    protected $connection = null;
+
+    /**
+     * @var mixed Connection variables
+     */
+    protected $numRows = 0;
+    protected $insertId = null;
+    protected $query = null;
+    protected $error = null;
+    protected $result = [];
+
+    /** 
+     * @var PDOStatement $statement The prepared statement after a query is set. 
+     */
+    protected PDOStatement $statement;
+
+    /**
+     * @var array SQL operators
+     */
+    protected $operators = ['=', '!=', '<', '>', '<=', '>=', '<>'];
+
+    /**
+     * @var int Total query count
+     */
+    protected $queryCount = 0;
+
+    /**
+     * @var bool Is it in the development environment?
+     */
+    protected $debug = true;
+
+    /**
+     * @var int Total transaction count
+     */
+    protected $transactionCount = 0;
+
   }
